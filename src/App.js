d64269e25e64f15aad169c8a42a9694463c82979
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-03 18:09:41 +0200

    add buttons to change account value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 function App() {
   const account = useSelector((state) => state.account);
-  console.log(account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
   return (
     <div className="App">
+      <button onClick={() => depositMoney(1000)}>deposit</button>
       <h1>{account}</h1>
+      <button onClick={() => withdrawMoney(2000)}>withdraw</button>
     </div>
   );
 }
