dce98c1eb7d7b28ff0d7d615e2d51182d19d8bb7
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-04 16:43:31 +0200

    clear app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import { bindActionCreators } from "redux";
 import { actionCreators } from "./state/index";
 
 function App() {
-  const account = useSelector((state) => state.account);
-  const dispatch = useDispatch();
 
-  const { depositMoney, withdrawMoney } = bindActionCreators(
-    actionCreators,
-    dispatch
-  );
 
   return (
     <div className="App">
-      <button onClick={() => depositMoney(1000)}>deposit</button>
+      <button >deposit</button>
       <h1>{account}</h1>
-      <button onClick={() => withdrawMoney(2000)}>withdraw</button>
+      <button >withdraw</button>
     </div>
   );
 }
--- src/state/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import thunk from "redux-thunk";
-import { applyMiddleware } from "redux";
-import reducers  from "./reducers/index.js";
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-
