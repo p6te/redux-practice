8137a213093610f54c1864793d947344dfb3e048
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-04 17:04:48 +0200

    recreate counter app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators } from "./state/index";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
 
+  const dispatch = useDispatch();
 
+  const minus = () => {
+    dispatch({
+      type: "MINUS",
+    });
+  };
+  const plus = () => {
+    dispatch({
+      type: "PLUS",
+    });
+  };
   return (
     <div className="App">
-      <button >deposit</button>
-      <h1>{account}</h1>
-      <button >withdraw</button>
+      <button onClick={() => plus()}>+ 1</button>
+      <h1>{counter}</h1>
+      <button onClick={() => minus()}>- 1</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+
+const reducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "PLUS":
+      return { counter: state.counter + 1 };
+
+    case "MINUS":
+      return { counter: state.counter - 1 };
+
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(reducer);
