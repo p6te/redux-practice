0893666deb918ce4b9288f2bc032b24645ddbd5e
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-05 11:54:55 +0200

    recreate app with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./store/features/counter/counterSlice";
 import { useState } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
 
+  const count = useSelector((state) => state.counter.value);
+
+  const dispatch = useDispatch();
+
+  const handleInputCounter = () => {
+    const inputNumber = parseInt(inputValue);
+    dispatch(incrementByAmount(inputNumber));
+    setInputValue("");
+  };
 
   return (
     <div className="App">
-      <button onClick={() => plus()}>+ 1</button>
-      <h1>{counter}</h1>
-      <button onClick={() => minus()}>- 1</button>
+      <button onClick={() => dispatch(increment())}>+ 1</button>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(decrement())}>- 1</button>
+      <br />
       <input
-        type="text"
+        type="number"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={() => handleInput()}>add input value</button>
+      <button onClick={() => handleInputCounter()}>add input value</button>
     </div>
   );
 }
