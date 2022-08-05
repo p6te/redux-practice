8fb279934a7cc7d6b0b6aa23243276ff1640a6aa
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-05 13:14:28 +0200

    starting template to shopping cart app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
-
   return (
     <div className="App">
-
+      <Auth />
+      {/* <Layout /> */}
     </div>
   );
 }
