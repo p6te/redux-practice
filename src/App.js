03181610d88243d46d343542114ab273ea107d80
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-05 14:09:06 +0200

    add login func

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <Auth />
-      <Layout />
-    </div>
-  );
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  console.log(isLoggedIn);
+
+  return <div className="App">{isLoggedIn ? <Layout /> : <Auth />}</div>;
 }
 
 export default App;
--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -15,6 +15,6 @@ const authSlice = createSlice({
   },
 });
 
-const authActions = authSlice.actions;
+export const authActions = authSlice.actions;
 
 export default authSlice;
