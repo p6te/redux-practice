0dc56693a7a0118b7dcb8f09a3ed8443ede32a0b
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-08-05 13:48:09 +0200

    create store

---
+++ src/store/auth-slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+const authActions = authSlice.actions;
+
+export default authSlice;
